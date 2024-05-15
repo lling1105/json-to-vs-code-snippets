67f84f313277471edcd0dc70ca9cbd871ad73b5d
luling <lling1105@gmail.com>
2024-05-15 17:12:37 +0800

    --

---
+++ index.js
@@ -0,0 +1,22 @@
+const fse = require('fs-extra');
+const path = require('path');
+const fs = require('fs');
+
+
+const root_folder_name = path.parse(process.cwd()).name;
+
+let code_snippets = {};
+fs.readdirSync(process.cwd()).forEach(file => {
+    let file_info = path.parse(file);
+    if(file_info.ext.toLowerCase()==='.json'){
+        let json_obj = fse.readJsonSync(path.join(process.cwd(),file));
+        code_snippets[`${file_info.name}`] = {
+            "scope":"javascript,typescript,json",
+            "prefix":"template",
+            "body":[
+                JSON.stringify(json_obj,null,'\t')
+            ]
+        }
+    }
+});
+fse.writeJsonSync(`${root_folder_name}.code-snippets`,code_snippets,{spaces:'\t'});
