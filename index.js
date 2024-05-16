b9be305378b8d52b1f073f357ac7743358c07ab2
luling <lling1105@gmail.com>
2024-05-16 17:25:38 +0800

    --

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fse = require('fs-extra');
 const path = require('path');
 const fs = require('fs');
 
+const one_line_templates_configs_file = path.join(process.cwd(), 'one_line_templates.json');
+const one_line_templates = fse.existsSync(one_line_templates_configs_file) ? fse.readJsonSync(one_line_templates_configs_file) : [];
 
 const root_folder_name = path.parse(process.cwd()).name;
 
@@ -10,15 +12,20 @@ fs.readdirSync(process.cwd()).forEach(file => {
     let file_info = path.parse(file);
     if(file_info.ext.toLowerCase()==='.json'){
         let json_obj = fse.readJsonSync(path.join(process.cwd(),file));
+        let body_string = JSON.stringify(json_obj, null, '\t')
+            .replace(/"\$/ig, '"\\$')
+            .replace(/{\n(\t)+/ig, '{')
+            .replace(/\n[\t]+}/ig, '}')
+            .replace(/\n}/ig, '}');
+        if (one_line_templates.includes(file_info.name)) { 
+            body_string = body_string.replace(/\n[\t]+/ig, '');
+        }
+        
         code_snippets[`${file_info.name}`] = {
             "scope":"javascript,typescript,json",
             "prefix":"template",
             "body":[
-                JSON.stringify(json_obj,null,'\t')
-                    .replace(/"\$/ig,'"\\$')
-                    .replace(/{\n(\t)+/ig, '{')
-                    .replace(/\n[\t]+}/ig, '}')
-                    .replace(/\n}/ig,'}')
+                body_string
             ]
         }
     }
